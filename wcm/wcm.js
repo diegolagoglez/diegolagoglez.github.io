574d6d31f0d633f0a4bb490407d83f9d882fd116
Diego Lago Gonzalez <diego.lago.gonzalez@gmail.com>
2021-03-24 20:14:57 +0100

    Add Working Calendar Manager page

---
+++ wcm/wcm.js
@@ -0,0 +1,34 @@
+
+function getURLFragment(defaultFragment) {
+    if (window.location.hash) {
+        return window.location.hash.substring(1);
+    }
+    return defaultFragment;
+}
+
+function modifyURLFragment(fragment) {
+    history.pushState(null,  null, fragment);
+}
+
+function doOnMenuClick(event) {
+    event.preventDefault();
+    $("section#start, section#working-help, section#faq").hide();
+    $("nav ul li a").removeClass("selected");
+    $(this).addClass("selected");
+    let section = $(this).attr("href");
+    $("section" + section).show();
+    modifyURLFragment(section);
+}
+
+$(function() {
+    $("nav ul li a").click(doOnMenuClick);
+    let fragment = getURLFragment();
+    if (typeof(fragment) == "undefined" || fragment == "" || fragment == "#start") {
+        $("section#working-help, section#faq").hide();
+    } else {
+        $("section#start, section#working-help, section#faq").hide();
+        $("nav ul li a").removeClass("selected");
+        $("nav ul li." + fragment + " a").addClass("selected");
+        $("section#" + fragment).show();
+    }
+})
